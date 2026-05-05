d9fcb5ef299629580197e9b531420cbb08ad3696
Leonardo <leonardogarcarr23@gmail.com>
2026-05-05 10:47:56 -0400

    fist configurations and primeNG imported

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,45 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeuix/themes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeuix/themes/aura';
+
+const MyPreset = definePreset(Aura, {
+  primitive: {
+    fontFamily: 'Inter, sans-serif' // Aquí defines la fuente global
+  },
+  semantic: {
+      primary: {
+          50: '{indigo.50}',
+          100: '{indigo.100}',
+          200: '{indigo.200}',
+          300: '{indigo.300}',
+          400: '{indigo.400}',
+          500: '{indigo.500}',
+          600: '{indigo.600}',
+          700: '{indigo.700}',
+          800: '{indigo.800}',
+          900: '{indigo.900}',
+          950: '{indigo.950}'
+      }
+  }
+});
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: MyPreset,
+          options: {
+            darkModeSelector: '.p-dark'
+          }
+      }
+  })
   ]
 };
